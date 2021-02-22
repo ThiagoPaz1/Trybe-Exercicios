c2a2937a2b9e4200213d614b4d0e3b91499378de
Thiago da Paz <thiagopazjunior@gmail.com>
2021-02-22 13:29:19 -0300

    Exercicios de 1 e 2.

---
+++ bloco_4/exercises2.js
@@ -0,0 +1,15 @@
+//1-Percorrer o array:
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+// for(i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+//2-Soma todos os valores do array:
+let somaNum = 0
+
+for(i = 0; i < numbers.length; i++){
+    somaNum += numbers[i]    
+}
+console.log(somaNum)
+
+
