b233165119390de7b2c592f50aba64723e4f16e5
Thiago da Paz <thiagopazjunior@gmail.com>
2021-02-24 13:11:49 -0300

    Adicionando exercicio 2.

--- bloco_4/exercises3.js
+++ bloco_4/exercises3.js
@@ -3,10 +3,16 @@ let n = 5;
 let contador = 0;
 let simbolo = '*';
 let quadrado = '';
+let triangulo = '';
 for(contador = 0; contador < n; contador++){
-  coluna = quadrado + simbolo;
+  quadrado = quadrado + simbolo;
 }
 for(contador = 0; contador < n; contador++){
   console.log(quadrado);
 }
 
+//2-Faça um triângulo de asteriscos:
+for(contador = 0; contador < n; contador++){
+  triangulo = triangulo + simbolo;
+  console.log(triangulo);
+}
